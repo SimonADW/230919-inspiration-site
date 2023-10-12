d713bf56abe1bb9010b6158a622c94358cc60348
SimonADW <simon-winter@outlook.com>
2023-10-12 13:04:52 +0200

    Set hamburger animation-(forwards)

--- js/script.js
+++ js/script.js
@@ -1,17 +1,30 @@
+// Drawer animation
 
 const hamburger = document.querySelector(".header__x-animation");
 const drawer = document.querySelector(".nav-drawer");
+const hamburgerXtop = document.querySelector(".header__x-animation-top-line");
+const hamburgerXbottom = document.querySelector(".header__x-animation-bottom-line");
+const hamburgerXcenter = document.querySelector(".header__x-animation-center-line");
+
 let isOpenDrawer = false;
 
 hamburger.addEventListener("click", ()=> {
-	drawer.style.animation = isOpenDrawer ? "drawerSlideUp 400ms forwards" : "drawerSlideDown  400ms forwards";
+	if(isOpenDrawer) {
+		drawer.style.animation = "drawerSlideUp 400ms forwards";
+		hamburgerXtop.style.animation = "x-animation-top 400ms ease-in-out 100ms reverse";
+		hamburgerXbottom.style.animation = "x-animation-bottom 400ms ease-in-out 100ms reverse";
+		hamburgerXcenter.style.opacity = "1";
+	} else {
+		drawer.style.animation = "drawerSlideDown 400ms forwards";
+		hamburgerXtop.style.animation = "x-animation-top 400ms ease-in-out 100ms forwards";
+		hamburgerXbottom.style.animation = "x-animation-bottom 400ms ease-in-out 100ms forwards";
+		hamburgerXcenter.style.opacity = "0";
+	}
 	isOpenDrawer = !isOpenDrawer;
-})
+});
 
 
-
-
-
+// Clips play on parent hover
 
 const playClip = function (clipNumber) {
 	let clip = document.getElementById(`clip${clipNumber}`);
